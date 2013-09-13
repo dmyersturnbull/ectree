ff2ca4746a921870f66249a9dd1546bfae5695ff
dmyersturnbull <dmyersturnbull@gmail.com>
2013-09-12 18:18:25 -0700

    Added an extra test and improved test speed.

--- src/test/java/org/ectree/IntegrationTest.java
+++ src/test/java/org/ectree/IntegrationTest.java
@@ -14,42 +14,28 @@
  */
 package org.ectree;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.NavigableSet;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class IntegrationTest {
 
-	@Test
-	public void testListOrder() throws IOException {
-		ECTree tree = ECTreeFactory.fromSibFile();
-		int length = 0;
-		ECNumber previous = null;
-		for (ECNode node : tree) {
-			if (node.isRoot()) continue;
-			if (node.getDepth() == length + 1) {
-			} else if (node.getDepth() == length) {
-				for (int i = 1; i < node.getDepth(); i++) {
-					assertTrue(node.getEcNumber() + " does not follow " + previous, node.getEcNumber().getCodeAtDepth(i) == previous.getCodeAtDepth(i));
-				}
-				assertTrue(node.getEcNumber().getCodeAtDepth(node.getDepth()) > previous.getCodeAtDepth(node.getDepth()));
-			} else if (node.getDepth() < length) {
-			} else {
-				fail("A length-" + node.getDepth() + " cannot follow a length-" + length + " (" + node.getEcNumber() + " followed " + previous + ")");
-			}
-			previous = node.getEcNumber();
-			length = previous.getDepth();
-			assertTrue(node.getDepth() >= length);
-		}
+	private ECTree tree;
+
+	@Before
+	public void setUp() throws IOException {
+		tree = ECTreeFactory.fromSibFile();
 	}
 
 	@Test
 	public void testBreadthFirst() throws IOException {
-		ECTree tree = ECTreeFactory.fromSibFile();
 		int length = 0;
 		ECNumber previous = null;
 		Iterator<ECNode> iter = tree.breadthFirst();
@@ -59,33 +45,63 @@ public class IntegrationTest {
 			if (node.getDepth() == length + 1) {
 			} else if (node.getDepth() == length) {
 			} else {
-				fail("A length-" + node.getDepth() + " cannot follow a length-" + length + " (" + node.getEcNumber() + " followed " + previous + ")");
+				fail("A length-" + node.getDepth() + " cannot follow a length-" + length + " (" + node.getEcNumber()
+						+ " followed " + previous + ")");
 			}
 			previous = node.getEcNumber();
 			length = previous.getDepth();
 			assertTrue(node.getDepth() >= length);
 		}
 	}
-	
-	@Test
-	public void testSubstringSearch() throws IOException {
-		ECTree tree = ECTreeFactory.fromSibFile();
-		NavigableSet<ECNode> nodes = tree.findByDescriptionSubstring("sulfur");
-		assertEquals("Wrong size", 21, nodes.size());
-		for (ECNode node : nodes) {
-			if (node.getEcNumber().toString().equals("1.3.7")) return;
-		}
-		fail("Didn't find 1.3.7");
-	}
-	
+
 	@Test
 	public void testExactSearch() throws IOException {
-		ECTree tree = ECTreeFactory.fromSibFile();
 		NavigableSet<ECNode> nodes = tree.findByExactDescription("Forming carbon-sulfur bonds.");
 		assertEquals("Wrong size", 1, nodes.size());
 		for (ECNode node : nodes) {
 			if (node.getEcNumber().toString().equals("6.2")) return;
 		}
 		fail("Didn't find 6.2");
+	}
+
+	@Test
+	public void testListOrder() throws IOException {
+		int length = 0;
+		ECNumber previous = null;
+		for (ECNode node : tree) {
+			if (node.isRoot()) continue;
+			if (node.getDepth() == length + 1) {
+			} else if (node.getDepth() == length) {
+				for (int i = 1; i < node.getDepth(); i++) {
+					assertTrue(node.getEcNumber() + " does not follow " + previous, node.getEcNumber()
+							.getCodeAtDepth(i) == previous.getCodeAtDepth(i));
+				}
+				assertTrue(node.getEcNumber().getCodeAtDepth(node.getDepth()) > previous
+						.getCodeAtDepth(node.getDepth()));
+			} else if (node.getDepth() < length) {
+			} else {
+				fail("A length-" + node.getDepth() + " cannot follow a length-" + length + " (" + node.getEcNumber()
+						+ " followed " + previous + ")");
+			}
+			previous = node.getEcNumber();
+			length = previous.getDepth();
+			assertTrue(node.getDepth() >= length);
+		}
+	}
+
+	@Test
+	public void testOfDepth() {
+		NavigableSet<ECNode> nodes = tree.findNodesOfDepth(2);
+		assertEquals("Wrong number", 67, nodes.size());
+	}
+
+	@Test
+	public void testSubstringSearch() throws IOException {
+		NavigableSet<ECNode> nodes = tree.findByDescriptionSubstring("sulfur");
+		assertEquals("Wrong size", 21, nodes.size());
+		for (ECNode node : nodes) {
+			if (node.getEcNumber().toString().equals("1.3.7")) return;
+		}
+		fail("Didn't find 1.3.7");
 	}
 }
